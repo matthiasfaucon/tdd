95c3ef5414cb865bf08c9a0f9b3ebe3a91c23399
Vincent <vincent@rouen-webmaster.com>
2022-10-17 10:24:17 +0200

    test unitaire pour une fonction sum et une fonction qui retourne une string et qui contient un mot en particulier

--- test/demoTest.js
+++ test/demoTest.js
@@ -1,8 +1,15 @@
 import { assert } from 'chai'
-import { sum } from '../demo/demo.js'
+import { sum, sayHello } from '../demo/demo.js'
 
 describe('sum()', function(){
     it('sum should return a+b', function(){
         assert.equal(sum(5,5), 10)
+    })
+})
+
+describe('sayHello()', function(){
+    it('function should return hello', function(){
+        assert.equal(sayHello(), 'hello')
+        assert.isString(sayHello(), 'c bien une string')
     })
 })
